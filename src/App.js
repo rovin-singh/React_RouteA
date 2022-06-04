0b1eaff34c3a47b3f17cbd4452583a7b4028c4ba
rovin-singh <91409265+rovin-singh@users.noreply.github.com>
2022-06-04 18:03:41 +0530

    "commit"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
-import Navbar from "./components/Navbar"
-import About from './components/About';
-import Home from './components/Home';
-import Contact from './components/Contact';
-import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Products from "./components/Products";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path="/contact" element={<Contact/>}></Route>
-          <Route path="/about" element={<About/>}></Route>
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Products />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
-import React from "react"
-import styles from "./style.module.css"
+import React from "react";
+import styles from "./style.module.css";
 import { Link } from "react-router-dom";
 
-const Navbar=()=>{
-    return(
-        <div className={styles.Navbar}>
-            <Link to="/" >Home</Link>
-            <Link to="/about" >About</Link>
-            <Link to="/contact" >Contact</Link>  
-        </div>
-    )
-}
+const Navbar = () => {
+  return (
+    <div className={styles.Navbar}>
+      <Link to="/">Home</Link>
+      <Link to="/products">Products</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </div>
+  );
+};
 export default Navbar;
